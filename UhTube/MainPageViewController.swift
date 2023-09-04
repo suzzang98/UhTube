b7a474ef9413e667f674a30042d13a757061706a
이시영 <jltldud@gmail.com>
2023-09-04 11:07:10 +0900

    TabBar Create

---
+++ UhTube/MainPageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainPageViewController.swift
+//  UhTube
+//
+//  Created by 이수현 on 2023/09/04.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("git test")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
