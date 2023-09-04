8372a619121f6e0ccfe0657e413bb6068f1b8eb2
riyeonlee <gjffod100@gmail.com>
2023-09-04 11:48:36 +0900

    riyeon branch create

--- UhTube/LoginPageViewController.swift
+++ UhTube/LoginPageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LoginPageViewController: UIViewController {
 
     override func viewDidLoad() {
+        print ("Hi")
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
